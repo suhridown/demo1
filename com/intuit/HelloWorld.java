082fc83eff379ba1037a60eb6bf83b09203225cc
Saran <ssaran@BANL128a2582c.local>
2016-03-21 16:11:09 +0530

    first commit

---
+++ com/intuit/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.intuit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"Hello");
+		System.out.println(
+				"Hello World");
+		
+
+	}
+
+}
